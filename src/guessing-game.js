911eb1f7cf1db078da9c6b40f7d5e1ee944f33fb
guchigow <lanov64@yandex.kz>
2023-02-13 01:26:33 +0600

    feat: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.cur = Math.floor((this.min + this.max + 1) / 2);
+        return this.cur;
     }
 
     lower() {
-
+        this.setRange(this.min, this.cur);
     }
 
     greater() {
-
+        this.setRange(this.cur, this.max);
     }
 }
 
